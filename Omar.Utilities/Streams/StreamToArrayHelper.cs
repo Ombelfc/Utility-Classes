6507841356d173324b8711c5a4f949d531869098
Omar <berraj97@gmail.com>
2019-01-02 11:02:24 +0100

    Added a stream to byte array method

---
+++ Omar.Utilities/Streams/StreamToArrayHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Omar.Utilities.Streams
+{
+    public static class StreamToArrayHelper
+    {
+        /// <summary>
+        /// Returns all bytes in a given Stream using the CopyTo method. Might have a performance issue.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static byte[] ReadAllBytes(Stream stream)
+        {
+            if (stream is MemoryStream)
+                return ((MemoryStream)stream).ToArray();
+
+            using (var mStream = new MemoryStream())
+            {
+                stream.CopyTo(mStream);
+                return mStream.ToArray();
+            }
+        }
+    }
+}
