d13f67a38a26069a09a097d60cf43b4debff0ed6
Omar <berraj97@gmail.com>
2019-01-14 10:11:15 +0100

    Added date-time helpers

---
+++ Omar.Utilities/Dates/DatesRangeHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Omar.Utilities.Dates
+{
+    public static class DatesRangeHelper
+    {
+        /// <summary>
+        /// Used to choose a random date from a collection of dates.
+        /// </summary>
+        private static Random random = new Random();
+
+        /// <summary>
+        /// Returns a list of dates from the given range.
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        public static IEnumerable<DateTime> DatesFromRange(this DateTime startDate, DateTime endDate)
+        {
+            return Enumerable.Range(0, (endDate - startDate).Days + 1).Select(d => startDate.AddDays(d));
+        }
+
+        /// <summary>
+        /// Returns a single date from the given range.
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        public static DateTime DateFromRange(this DateTime startDate, DateTime endDate)
+        {
+            var dates = Enumerable.Range(0, (endDate - startDate).Days + 1).Select(d => startDate.AddDays(d)).ToList();
+            return dates[random.Next(dates.Count())];
+        }
+    }
+}
---
+++ Omar.Utilities/Json/JsonPrintingHelper.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Omar.Utilities.Json
+{
+    public static class JsonPrintingHelper
+    {
+        public static string DumpAsJson(this IEnumerable obj)
+        {
+            return JsonConvert.SerializeObject(obj, Formatting.Indented);
+        }
+    }
+}
