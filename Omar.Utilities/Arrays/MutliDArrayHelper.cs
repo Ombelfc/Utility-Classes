2eeb16c5b7ac099ff484ccf999728729e26a7c61
Omar <berraj97@gmail.com>
2019-01-31 10:38:27 +0100

    Added a couple 2d array helpers to retrieve a row/column

---
+++ Omar.Utilities/Arrays/MutliDArrayHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace Omar.Utilities.Arrays
+{
+    public static class MultiDArrayHelper
+    {
+        /// <summary>
+        /// Returns an array with all the values from the given column number.
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="columnNumber"></param>
+        /// <returns></returns>
+        public static string[] GetColumn(this string[,] matrix, int columnNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(0))
+                    .Select(x => matrix[x, columnNumber])
+                    .ToArray();
+        }
+
+        /// <summary>
+        /// Returns an array with all the values from the given row number.
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="rowNumber"></param>
+        /// <returns></returns>
+        public static string[] GetRow(this string[,] matrix, int rowNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(1))
+                    .Select(x => matrix[rowNumber, x])
+                    .ToArray();
+        }
+    }
+}
--- Omar.Utilities/Streams/UtilityClasses/FileConsoleLog.cs
+++ Omar.Utilities/Streams/UtilityClasses/FileConsoleLog.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace Omar.Utilities.Streams
+namespace Omar.Utilities.Streams.UtilityClasses
 {
     public class FileConsoleLog
     {
